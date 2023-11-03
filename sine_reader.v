6e206b2f14821c62bda28677b358136b73b18fbd
cristaolan <147454831+cristaolan@users.noreply.github.com>
2023-11-02 21:34:14 -0700

    Update sine_reader.v

--- sine_reader.v
+++ sine_reader.v
@@ -8,9 +8,9 @@ module sine_reader(
     output wire [15:0] sample
 );
     wire signed [21:0] cur_addr; // {quadrant. address, precision}
+    wire signed [21:0] cur_addr_temp;
     wire signed [21:0] next_addr; 
     wire signed [15:0] sample_out; // reg to go into sine_rom
-    reg signed [21:0] next_value; // used to calculate next_addr within case statement
     
     // initialize DFF 
     dffre #(.WIDTH(22)) counter (.clk(clk), .r(reset), .d(next_addr), .q(cur_addr), .en(generate_next));
@@ -18,16 +18,20 @@ module sine_reader(
     // increment the current address: if we are in quadrants 00 or 10, increment your
     // address through the ROM, else, decrement your address through the ROM
     
-    assign next_addr = cur_addr + {2'b00, step_size};
-    assign next_addr = (cur_addr[20]) ? 20'd0 - next_addr[19:10]: next_addr[19:10];
-                                 
+    assign next_addr = cur_addr + {2'b00, step_size}; // keep incrementing the address by the step size to change the quadrant naturally
+    
+    assign cur_addr_temp = (cur_addr[20]) ? {cur_addr[21:20], ~cur_addr[19:0]} : cur_addr; // for quadrants 01 and 11, invert bits
+    
+        
+                                        
     // step through sine ROM, only taking first 10 bits of cur_addr as our address
-    sine_rom rom_instance (.clk(clk), .addr(cur_addr[19:10]), .dout(sample_out));
+    sine_rom rom_instance (.clk(clk), .addr(cur_addr_temp[19:10]), .dout(sample_out));
     
     // need to invert the output depending on the quadrant + we can only output a sample
     // when generate_next is high
      assign sample = (cur_addr[21]) ? 
-                    -sample_out : sample_out;
+                   -sample_out : sample_out;
+
      
 
      assign sample_ready = (generate_next);
@@ -35,7 +39,6 @@ module sine_reader(
   
 
 endmodule
-
 ____________________
 // module sine_reader(
 //     input clk,
