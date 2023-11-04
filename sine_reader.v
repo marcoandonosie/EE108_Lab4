ca17f8c3545d28dd571ca06dbe5c711e1cd63f26
cristaolan <147454831+cristaolan@users.noreply.github.com>
2023-11-03 23:20:15 -0700

    Update sine_reader.v

--- sine_reader.v
+++ sine_reader.v
@@ -18,11 +18,9 @@ module sine_reader(
     // increment the current address: if we are in quadrants 00 or 10, increment your
     // address through the ROM, else, decrement your address through the ROM
     
-    assign next_addr = cur_addr + {2'b00, step_size}; // keep incrementing the address by the step size to change the quadrant naturally
+    assign next_addr = (reset) ? 22'b0 : cur_addr + {2'b00, step_size}; // keep incrementing the address by the step size to change the quadrant naturally
     
-    assign cur_addr_temp = (cur_addr[20]) ? {cur_addr[21:20], ~cur_addr[19:0]} : cur_addr; // for quadrants 01 and 11, invert bits
-    
-        
+    assign cur_addr_temp = (cur_addr[20]) ? {cur_addr[21:20], ~cur_addr[19:0]} : cur_addr; // for quadrants 01 and 11, invert bits       
                                         
     // step through sine ROM, only taking first 10 bits of cur_addr as our address
     sine_rom rom_instance (.clk(clk), .addr(cur_addr_temp[19:10]), .dout(sample_out));
@@ -33,12 +31,16 @@ module sine_reader(
                    -sample_out : sample_out;
 
      
-
-     assign sample_ready = (generate_next);
-     
-  
+    // delay sample_ready by two clock cycles 
+    wire d1 = (generate_next);
+    wire q1; 
+    
+    dffr dff1 (.clk(clk), .r(reset), .d(d1), .q(q1));
+    dffr dff2 (.clk(clk), .r(reset), .d(q1), .q(sample_ready)); 
+   
 
 endmodule
+
 ____________________
 // module sine_reader(
 //     input clk,
