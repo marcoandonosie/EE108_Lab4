ef3b9819856de67272df16481ce1201c12b3d690
marcoandonosie <147224708+marcoandonosie@users.noreply.github.com>
2023-11-04 19:30:53 -0700

    Update song_reader_tb.v

--- song_reader_tb.v
+++ song_reader_tb.v
@@ -34,13 +34,55 @@ module song_reader_tb();
         note_done = 1'd1;
         #10
         note_done = 1'd0;
+        //testing to see if address increments by one; should print note at address 00 00001
         $display ("Playing a note of song %d, which is note %d duration %d", song, note, duration);
+        $display ("expected song 00, note 1, duration 8");
         
+        note_done = 1'd1;
         #10
+        note_done = 1'd0;
+        //should be at address 00 00002; testing address incrementing 
+        $display ("Playing a note of song %d, which is note %d duration %d", song, note, duration);
+        $display ("expected song 00, note 51, duration 12");
+        #10
+
+        note_done = 1'd1;
+        #10
+        note_done = 1'd0;
+        #10
+        note_done = 1'd1;
+        #10
+        note_done = 1'd0;
+        //should be at address 00 00004; testing address incrementing 
+        $display ("Playing a note of song %d, which is note %d duration %d", song, note, duration);
+        $display ("expected song 00, note 52, duration 12");
+
+        //testing the reset button; should reset to first note address of song 0
+        reset = 1'b1;
+        #10
+        reset ' 1'b0
+        $display ("Playing a note of song %d, which is note %d duration %d", song, note, duration);
+        $display ("expected song 00, note 49, duration 12");
+        
+        
+        //testing change song, should be in 01 00000
         song = 2'b01;
+        $display ("Playing a note of song %d, which is note %d duration %d", song, note, duration);
+        $display ("expected song 00, note 35, duration 36");
+
+        #10
+        //testing pause, nothing should change 
         play = 1'b0;
         #10
         $display("Playing a note of song %d, which is note %d duration %d", song, note, duration);
+        $display ("expected song 00, note 35, duration 36");
+
+        play = 1'b1;
+        //testing fourth song
+        #10
+        song = 2'd3 
+        $display("Playing a note of song %d, which is note %d duration %d", song, note, duration);
+        $display ("expected song 00, note 40, duration 20");
     end
 
 endmodule
